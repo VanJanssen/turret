03840319c83651ea9ae9a5e9c4f9960f59df78dd
Erwin Janssen <erwinjanssen@outlook.com>
2017-12-29 20:52:26 +0100

    Remove invocation of main() from turret.cli
    Invoking the main command line is done via setuptools integration, so the
    invocation of main() in `if __name__ == "__main__"` is redundant.

--- turret/cli.py
+++ turret/cli.py
@@ -11,7 +11,3 @@ def main(args=None):
     click.echo("Replace this message by putting your code into "
                "turret.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
-
-
-if __name__ == "__main__":
-    main()
