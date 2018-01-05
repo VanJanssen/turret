30e2ab49c9e3a98dbdb65ccb09e58be5ef5d3146
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-05 09:18:48 +0100

    Add --version option to cli
    The --version option is provided by added the @@click.version_option
    decorator to cli.main(). The default message is overridden, so the
    output will be something like 'turret 0.1.0' instead of
    'turret, version 0.1.0'.

--- turret/cli.py
+++ turret/cli.py
@@ -6,6 +6,7 @@ import click
 
 
 @click.command()
+@click.version_option(message='%(prog)s %(version)s')
 def main(args=None):
     """Console script for turret."""
     click.echo("Replace this message by putting your code into "
