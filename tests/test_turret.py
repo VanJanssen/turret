a4d5889dc9bc4128dd2a58c8c6e3c5ee4198b139
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-08 14:59:05 +0100

    Test cases for main and raw CLI groups added
    These test cases check the exit codes and output for the commands:
    - `turret`
    - `turret --help`
    - `turret --version`
    - `turret raw`
    - `turret raw --help`

--- tests/test_turret.py
+++ tests/test_turret.py
@@ -5,10 +5,6 @@
 
 import pytest
 
-from click.testing import CliRunner
-
-from turret import cli
-
 
 @pytest.fixture
 def response():
@@ -24,16 +20,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert result.output == help_result.output
-    assert '--version  Show the version and exit.' in help_result.output
-    assert '--help     Show this message and exit.' in help_result.output
-    assert 'scout  Perform reconnaissance.' in help_result.output
