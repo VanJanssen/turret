a64070c26b796e064b2fc35b0cc316d218107a10
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-16 15:47:13 +0100

    Short description added to nmap module

---
+++ turret/raw/nmap/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Raw module for Nmap.
+
+This module provides direct access to Nmap, but also to the wrapper and
+utility functions added by Turret. All functions can be access through the
+API and there is a dedicated nmap CLI subcommand.
+"""
