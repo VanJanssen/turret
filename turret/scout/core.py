d6f37f8145aafe3e14b71b7878da719ee3c6f4c6
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-16 15:13:16 +0100

    New signature for scout.recon, but disable for now
    The new signature and added docstring provides a short description about
    what this function is supposed to do when it gets implemented. For now
    it is disabled (the body replaced by a 'pass'), because just calling
    nmap is now the responsibility of the nmap module.

--- turret/scout/core.py
+++ turret/scout/core.py
@@ -3,6 +3,14 @@
 """Utilities to perform reconnaissance on remote targets."""
 
 
-def recon(arguments):
-    from turret.raw.nmap.nmap import scan
-    scan(arguments)
+def recon(targets, configuration):
+    """
+    Perform reconaicance on specified targets.
+
+    High level function that will perform reconaicance on the targets with the
+    specified configuration. This configuration can be 'quick', 'intens',
+    'slow' or 'stealth'. Based on the configuration, recon will perform
+    various tasks and also call other tools in order to provide as much
+    information as possible about the targets.
+    """
+    pass
