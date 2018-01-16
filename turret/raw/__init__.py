9efc94db5111d1f0addd20a5e224487d46c1be0d
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-16 15:33:43 +0100

    Add explaination of the raw module in docstring
    The file turret/raw/__init__.py now contains a short explaination of the
    purpose and usage of the raw module.

---
+++ turret/raw/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Raw components of Turret.
+
+To perform its tasks, Turret uses various external tools. For example,
+instead of implementing its own network scanner, Turret leverages the power
+of the highly advanced Nmap. Turret invokes this tool with various preset
+arguments, parses its output and acts upon that. This module contains the
+wrappers around these tools, as used internally by Turret. However, these
+wrapper can also be used by other users, as Turret exposes this as part of its
+API, and a dedicated CLI command is available for every tool.
+"""
