e0348e11917132ecb4b3b3f6ea13f00d5d6f67a6
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-30 13:25:35 +0100

    Improve docstrings and add typing to extenders.py
    This is the first file where optional type annotations are added. It
    passes `mypy --strict`. The improved docstrings also pass the pydocstyle
    checks.

--- turret/core/extenders.py
+++ turret/core/extenders.py
@@ -3,23 +3,29 @@
 """Utilities to extend Turret functionality."""
 
 import subprocess
+from typing import List, Optional
 
 
 class Program():
     """Interface for executing and extending arbitrary programs.
 
     This class provides a way to execute an arbitrary program and to extend
-    its functionality with additonal options and output formats.
+    its functionality with additonal options and output formats. This base
+    class adds options and features which are supported by most programs. More
+    complex extentsion can be provided through subclasses.
     """
 
-    def __init__(self, executable, arguments=list(), *,
-                 show_help=False,
-                 help_option='--help',
-                 show_version=False,
-                 version_option='--version'):
-        """Basic boilerplate for Program object creation.
+    def __init__(self,
+                 executable: str,
+                 arguments: Optional[List[str]] = None,
+                 *,
+                 show_help: bool = False,
+                 help_option: str = '--help',
+                 show_version: bool = False,
+                 version_option: str = '--version') -> None:
+        """Initialize basic Program objects.
 
-        The only required parameter is the eversionxecutable name. Additional
+        The only required parameter is the executable name. Additional
         arguments can be directly provided, but they can also be added later
         or with the use of options. Some basic options, which are supported by
         most programs, are included in this constructor.
@@ -28,10 +34,8 @@ class Program():
             executable: Name of the program to execute. It will be passed
                 directly to the subprocess call, so it can be a str, bytes or
                 os.PathLike object.
-            arguments (list): List of additonal arguments that will be directly
-                passed to the subprocess call. No additonal checking will be
-                performed, except that it will be converted to a list if it is
-                not already one.
+            arguments: List of additonal arguments that will be directly
+                passed to the subprocess call.
             show_help (bool): Boolean to indicate if the program should show
                 its help message. This will append the value of 'help_option'
                 to the command when executing the program.
@@ -44,25 +48,37 @@ class Program():
                 for it to display the version message. Defaults to '--version'.
         """
         self.executable = executable
-        self.arguments = arguments
+        self.arguments = arguments if arguments is not None else list()
         self.show_help = show_help
         self.help_option = help_option
         self.show_version = show_version
         self.version_option = version_option
 
-    def run(self):
+    def run(self) -> subprocess.CompletedProcess:
+        """Run the program with the constructed command."""
         return subprocess.run(self.command)
 
     @property
-    def command(self):
-        """Constructs the command based on the executable, arguments and
-        additonal options provided."""
+    def command(self) -> List[str]:
+        """Construct the command to be executed.
+
+        This read-only property constructs the command based on the executable,
+        arguments and additonal options provided. Subclasses that implement
+        this, should start with '_command = super().command'. This takes care
+        of the basic options.
+
+        Returns:
+            A list which can be passed to the subprocess module. The first
+            element is the 'executable', followed by the 'arguments'.
+            Additional values are appended based on the enabled options.
+
+        """
         _command = [self.executable] + self.arguments
 
         if self.show_help:
-            _command += [self.help_option]
+            _command.append(self.help_option)
 
         if self.show_version:
-            _command += [self.version_option]
+            _command.append(self.version_option)
 
         return _command
