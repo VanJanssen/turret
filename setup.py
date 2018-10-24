5750fecf7531660fbefc10b5788d35286bdf5f47
Erwin Janssen <erwinjanssen@outlook.com>
2018-10-24 16:32:43 +0200

    Move project config from setup.py to setup.cfg (#69)
    Instead of using the setup.py script, a plain test configuration file
    can also be used. This file reduces boilerplate and is more structured
    than setup.py, while the end result is the same.

--- setup.py
+++ setup.py
@@ -1,60 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""The setup script."""
+"""Setuptools installation entry point.
 
-from setuptools import setup, find_packages
+Besides from the mandatory `setup()` call, this file is empty in favor of
+storing the configuration in the `setup.cfg` file.
+"""
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+from setuptools import setup
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    'Click>=6.0',
-    'psutil>=4.4.0',
-    'xmltodict',
-]
-
-setup_requirements = [
-    'pytest-runner',
-]
-
-test_requirements = [
-    'pytest',
-    'pytest-cov',
-]
-
-setup(
-    name='turret',
-    version='0.1.0',
-    description="Discover, analyse, manage, attack and defend computer networks.",
-    long_description=readme + '\n\n' + history,
-    author="Erwin Janssen",
-    author_email='erwinjanssen@outlook.com',
-    url='https://github.com/VanJanssen/turret',
-    packages=find_packages(include=['turret']),
-    entry_points={
-        'console_scripts': [
-            'turret=turret.core.cli:main'
-        ]
-    },
-    include_package_data=True,
-    install_requires=requirements,
-    license="MIT license",
-    zip_safe=False,
-    keywords='turret',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
-)
+setup()
